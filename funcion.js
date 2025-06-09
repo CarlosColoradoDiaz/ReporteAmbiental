b1895f8b1ee73a9d0eeb9633673b263c0035ee24
Carlos Colorado <carlosandrescolorado09@gmail.com>
2025-06-09 14:57:03 -0500

    Add files via upload

--- funcion.js
+++ funcion.js
@@ -1,13 +1,14 @@
 //MOSTRAR EL FORMULARIO Y ASIGNARLE UN BACKGROUND
 document.getElementById("boton-form").addEventListener("click", function() { //ADDEVENTLISTENER, PARA QUE OCURRA UN EVENTO DE TIPO "CLICK"
     document.getElementById("formulario").style.display = "block"; //MOSTRAR EL FORMULARIO MEDIANTE EL DISPLAY "BLOCK" LUEGO DE QUE YA EL USUARIO HAYA DADO "CLICK"
-    document.getElementById("body").style.background = "rgba(5, 7, 12, 0.60)" //AL HACER "CLICK" SE AGREGA UN FONDO DE COLOR OPACO PARA QUE SE VEA VISUALMENTE MUY BIEN EL FORMULARIO
+    document.getElementById("body").style.background ="rgba(40, 80, 50, 0.9)" //AL HACER "CLICK" SE AGREGA UN FONDO DE COLOR OPACO PARA QUE SE VEA VISUALMENTE MUY BIEN EL FORMULARIO
 });
 
 //CANCELAR FORMULARIO
 document.getElementById("cancelar").addEventListener("click", function() { //ADDEVENTLISTENER, PARA QUE OCURRA UN EVENTO DE TIPO "CLICK"
     document.getElementById("formulario").style.display = "none"; //LLAMOS EL ID FORMULARIO Y DESPUÉS APLICAMOS DISPLAY "NONE" PARA ESCONDER EL FORMULARIO LUEGO DE LA EJECUCIÓN
-    document.getElementById("body").style.background = "white" //AL HACER "CLICK" SE AGREGA UN FONDO DE COLOR BLANCO PARA QUE EL BODY SE VEA NUEVAMENTE COMO ESTABA 
+    document.getElementById("body").style.background = "#a8ffaf" //AL HACER "CLICK" SE AGREGA EL FONDO VERDE PARA QUE EL BODY SE VEA NUEVAMENTE COMO ESTABA 
+    document.getElementById('evidencias').style.zIndex="1"
 });
 
 //ENVIAR EL FORMULARIO
@@ -25,23 +26,36 @@ document.getElementById("enviar").addEventListener("click", function(e) { //ADDE
     if (numero === "" || departamento === "" || fecha === "" || foto === "" || direccion === "" || queja === "") { //SI EL USUARIO NO RELLENA UNA DE ESTAS VARIABLES LE SALE UNA ALERTA
         alert("Hay campos obligatorios que están vacíos.");
     } else {
+        guardarDatos()
         alert("Tu denuncia ha sido registrada. Nos estás ayudando a proteger el medio ambiente."); //SI EL USUARIO INGRESA TODO, TODO ES EXITOSO
-
-        document.getElementById("body").style.background = "white" //AL HACER "CLICK" SE AGREGA UN FONDO DE COLOR BLANCO PARA QUE EL BODY SE VEA NUEVAMENTE COMO ESTABA 
+        document.getElementById("body").style.background = "#a8ffaf" //AL HACER "CLICK" SE AGREGA UN FONDO DE COLOR BLANCO PARA QUE EL BODY SE VEA NUEVAMENTE COMO ESTABA 
         document.querySelector("form").reset(); //LLAMAMOS LA ETIQUETA FORM Y DESPUÉS EL MÉTODO RESET PARA LIMPIAR EL FORMULARIO LUEGO DE LA EJECUCIÓN 
         document.getElementById("formulario").style.display = "none"; //LLAMOS EL ID FORMULARIO Y DESPUÉS APLICAMOS DISPLAY "NONE" PARA ESCONDER EL FORMULARIO LUEGO DE LA EJECUCIÓN
     }
 });
 
-//USO DE LA API DE DEPARTAMENTOS
+//PARA GUARDAR LOS DATOS INGRESADOS POR EL USUARIO
+function guardarDatos(){
+    const datos = {
+    numeros: document.getElementById('numero').value,
+    departamentos:document.getElementById("departamento").value,
+    fechas:document.getElementById("fecha").value,
+    fotos:document.getElementById("foto").value,
+    direcciones:document.getElementById("direccion").value,
+    quejas:document.getElementById("queja").value
+}
+console.log(datos)
+}
+
+//USO DE LA API PARA LOS DEPARTAMENTOS
 fetch('https://api-colombia.com/api/v1/Department')
 .then(resp =>resp.json())
 .then(array => {
-    let select = document.getElementById('departamento');
+    let select = document.getElementById('departamento')
     for(let i=0; i<array.length; i++){
-        console.log(array[i].name);
-        let option = document.createElement('option');
-        option.textContent = array[i].name;
-        select.appendChild(option);
-    }
+        console.log(array[i].name)
+        let option = document.createElement('option')
+        option.textContent = array[i].name
+        select.appendChild(option)
+        }
 });
